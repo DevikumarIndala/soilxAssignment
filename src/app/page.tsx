3b31b8bb5e56a0717de92ef7ad910ac6ecf550b0
Devi Kumar  Indala <devikumar.i@arthink.ai>
2024-05-24 06:35:40 +0530

    the check1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import HomeComponent from "@/components/home";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col dark:bg-slate-950">
-      <Counter/>
+      {/* <Counter/> */}
       <HomeComponent />
     </main>
   );
